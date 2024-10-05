b65ba743f24deef236cd565f3a5273b7a9f3c2c4
miya2009 <miya_2009@naver.com>
2024-10-05 17:18:42 +0900

    5장 프로젝트 bootstrap 화면처리

---
+++ src/main/java/com/pgm/springex2/config/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package com.pgm.springex2.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+
+public class ModelMapperConfig {
+    public ModelMapper getMapper(){
+        ModelMapper modelMapper=new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+        return modelMapper;
+    }
+}
